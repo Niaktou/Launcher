a8d0b982a1c47023d3da1366783d50c12473be1e
Niaktou <remi.debout@gmail.com>
2023-04-02 17:44:22 +0200

    v2

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -23,8 +23,8 @@ class Splash {
     async startAnimation() {
         let splashes = [
             { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Salut je suis le launcher", "author": "Luuxis" },
+            { "message": "Je suis ton père", "author": "Luuxis" }
         ]
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
